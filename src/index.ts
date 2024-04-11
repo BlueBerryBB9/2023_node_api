9aa835c263ccb4c55168f84e30960795303a52a0
Martin Leroy <martin.leroy@ecole-89.com>
2024-04-11 15:33:04 +0200

    scripts and added dist to gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 interface Animal {
-  make_sound(): void;
+    make_sound(): void;
 }
 
 class Cat {
-  make_sound() {
-    console.log("meow");
-  }
+    make_sound() {
+        console.log("meow");
+    }
 }
 
 class Dog {
-  make_sound() {
-    console.log("woof");
-  }
+    make_sound() {
+        console.log("woof");
+    }
 }
 
 function call_make_sound(animal: Animal) {
-  animal.make_sound();
+    animal.make_sound();
 }
 
 const cat = new Cat();

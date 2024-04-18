d74ec4be85677874df109562246a996893925072
Martin Leroy <martin.leroy@ecole-89.com>
2024-04-18 17:32:51 +0200

    exo6 done

--- src/exo5.test.ts
+++ src/exo5.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "@jest/globals";
 import { TimeSlot, mk_slots } from "./exo5.js";
 
-describe("exo4 module", () => {
+describe("exo5 module", () => {
     test("mk_slots", () => {
         expect(
             mk_slots({
@@ -35,6 +35,82 @@ describe("exo4 module", () => {
             {
                 bgn_date: new Date(2024, 3, 18, 13, 14),
                 end_date: new Date(2024, 3, 18, 13, 29),
+            },
+        ]);
+    });
+    test("mk_slots several + space", () => {
+        expect(
+            mk_slots({
+                date: new Date(2024, 3, 18, 12, 44),
+                inc: 15,
+                slot_nb: 3,
+                space: 3,
+            }),
+        ).toStrictEqual([
+            {
+                bgn_date: new Date(2024, 3, 18, 12, 44),
+                end_date: new Date(2024, 3, 18, 12, 59),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 13, 2),
+                end_date: new Date(2024, 3, 18, 13, 17),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 13, 20),
+                end_date: new Date(2024, 3, 18, 13, 35),
+            },
+        ]);
+    });
+    test("mk_slots several pause", () => {
+        expect(
+            mk_slots({
+                date: new Date(2024, 3, 18, 12, 44),
+                inc: 15,
+                slot_nb: 3,
+                pause_rate: 2,
+                pause_time: 30,
+            }),
+        ).toStrictEqual([
+            {
+                bgn_date: new Date(2024, 3, 18, 12, 44),
+                end_date: new Date(2024, 3, 18, 12, 59),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 12, 59),
+                end_date: new Date(2024, 3, 18, 13, 14),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 13, 44),
+                end_date: new Date(2024, 3, 18, 13, 59),
+            },
+        ]);
+    });
+    test("mk_slots several space + pause", () => {
+        expect(
+            mk_slots({
+                date: new Date(2024, 3, 18, 12, 44),
+                inc: 15,
+                slot_nb: 4,
+                pause_rate: 2,
+                pause_time: 30,
+                space: 3,
+            }),
+        ).toStrictEqual([
+            {
+                bgn_date: new Date(2024, 3, 18, 12, 44),
+                end_date: new Date(2024, 3, 18, 12, 59),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 13, 2),
+                end_date: new Date(2024, 3, 18, 13, 17),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 13, 47),
+                end_date: new Date(2024, 3, 18, 14, 2),
+            },
+            {
+                bgn_date: new Date(2024, 3, 18, 14, 5),
+                end_date: new Date(2024, 3, 18, 14, 20),
             },
         ]);
     });
--- src/exo5.ts
+++ src/exo5.ts
@@ -11,23 +11,33 @@ export function mk_slots(params: {
     inc: number;
     slot_nb: number;
     space?: number;
+    pause_rate?: number;
+    pause_time?: number;
 }) {
-    let i = 0;
     let slots_array: TimeSlot[] = [];
-
     let new_date: Date = new Date(params.date);
     new_date.setMinutes(new_date.getMinutes() + params.inc);
     let dates: Date[] = [params.date, new_date];
 
-    while (i < params.slot_nb) {
+    if (params.space === undefined) params.space = 0;
+    if (params.pause_rate === undefined) params.pause_rate = 0;
+    if (params.pause_time === undefined) params.pause_time = 0;
+
+    for (let i = 1; i <= params.slot_nb; i++) {
+        slots_array.push({ bgn_date: dates[0], end_date: dates[1] });
+        dates = mk_date({
+            date: new Date(dates[1]),
+            inc:
+                (i % params.pause_rate === 0 && i != 0
+                    ? params.pause_time
+                    : 0) || (params.space !== 0 ? params.space : 0),
+            date_nb: 1,
+        });
         dates = mk_date({
             date: new Date(dates[1]),
             inc: params.inc,
             date_nb: 1,
         });
-        slots_array.push({ bgn_date: dates[0], end_date: dates[1] });
-        i++;
     }
-
     return slots_array;
 }

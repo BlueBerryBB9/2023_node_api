874b83969a22b2c66d46b0d16ef10f942018fc0d
Martin Leroy <martin.leroy@ecole-89.com>
2024-04-18 15:16:32 +0200

    exo5 done

--- src/exo5.ts
+++ src/exo5.ts
@@ -1,4 +1,4 @@
-import { mk_dates_every_minutes } from "./exo4.js";
+import { mk_dates_every_minutes as mk_date } from "./exo4.js";
 
 export type TimeSlot = {
     bgn_date: Date;
@@ -9,17 +9,18 @@ export type TimeSlot = {
 export function mk_slots(params: { date: Date; inc: number; slot_nb: number }) {
     let i = 0;
     let slots_array: TimeSlot[] = [];
+
     let new_date: Date = new Date(params.date);
     new_date.setMinutes(new_date.getMinutes() + params.inc);
     let dates: Date[] = [params.date, new_date];
 
     while (i < params.slot_nb) {
-        slots_array.push({ bgn_date: dates[0], end_date: dates[1] });
-        dates = mk_dates_every_minutes({
+        dates = mk_date({
             date: new Date(dates[1]),
             inc: params.inc,
             date_nb: 1,
         });
+        slots_array.push({ bgn_date: dates[0], end_date: dates[1] });
         i++;
     }
 

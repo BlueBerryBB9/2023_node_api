effba3570d27755dc2a4420cb46ef0413d323f11
Martin Leroy <martin.leroy@ecole-89.com>
2024-05-23 16:54:22 +0200

    TD4 first exercices done

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # 2023_node_api
 
 **recent changes :**
-- step5 td3 done
+
+- TD3
 
 **current tasks :**
-- step 6 TD3
+
+- TD4
 - ask how do we need to specify the span (as parameters : by id or by a Span to create) where adding slots in
-- add checks for nonsense dates when creating Span or Slot (e.g : the start date after the end date , start and end dates are equal, ...)
-- NOT DONE : add checks in mk_slots (reservation) if (dates of a slot > the end date of the span) and then stop filling the span with slots
--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,66 @@
 import Fastify, * as ff from "fastify";
 let web_server = Fastify({ logger: true });
 
-web_server.get("/", async () => {
-    return { message: "hello world" };
-});
+export function start_web_server() {
+    web_server.get("/", async () => {
+        return { message: "hello world" };
+    });
 
-type Operation = {
-    kind: "ADD" | "SUB" | "MUL" | "DIV" | "MOD";
-    rhs: number;
-    lhs: number;
-    res: number;
-};
-
-const operations: number[] = [];
-web_server.post<{
-    Body: {
+    type Operation = {
         kind: "ADD" | "SUB" | "MUL" | "DIV" | "MOD";
         rhs: number;
         lhs: number;
+        res: number;
     };
-}>("/operations", async (req, res) => {
-    const { kind, rhs, lhs } = req.body;
-    let result: number;
 
-    if (kind === "ADD") {
-        result = lhs + rhs;
-    } else if (kind === "SUB") {
-        result = lhs - rhs;
-    } else if (kind === "MUL") {
-        result = lhs * rhs;
-    } else if (kind === "DIV") {
-        result = lhs / rhs;
-    } else if (kind === "MOD") {
-        result = lhs % rhs;
-    } else {
-        return res.status(400).send({ error: "Invalid" });
-    }
-    const id = operations.push(result);
+    const operations: Operation[] = [];
 
-    return res.status(201).send({ id, message: "created" });
-});
+    web_server.post<{
+        Body: Omit<Operation, "res">;
+    }>("/operations", async (req, res) => {
+        const { kind, rhs, lhs } = req.body;
+        let result: number;
 
-web_server.listen({ port: 1234, host: "0.0.0.0" }, (err, address) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`listening on ${address}`);
-    }
-});
+        if (kind === "ADD") {
+            result = lhs + rhs;
+        } else if (kind === "SUB") {
+            result = lhs - rhs;
+        } else if (kind === "MUL") {
+            result = lhs * rhs;
+        } else if (kind === "DIV") {
+            result = lhs / rhs;
+        } else if (kind === "MOD") {
+            result = lhs % rhs;
+        } else {
+            return res.status(400).send({ error: "Invalid" });
+        }
+        const id = operations.push({ kind, rhs, lhs, res: result });
+
+        return res.status(201).send({ id, message: "created" });
+    });
+
+    web_server.get("/operations", async (req, res) => {
+        return res.status(201).send({ data: operations, message: "success" });
+    });
+
+    web_server.get<{ Params: { id: number } }>(
+        "/operations/:id",
+        async (req, res) => {
+            let id: number = req.params.id;
+
+            if (isNaN(id) || id < 1 || id > operations.length) {
+                return res.status(404).send({ error: "Invalid id" });
+            }
+
+            return res.status(200).send(operations[id - 1]);
+        },
+    );
+
+    web_server.listen({ port: 1234, host: "0.0.0.0" }, (err, address) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`listening on ${address}`);
+        }
+    });
+}
